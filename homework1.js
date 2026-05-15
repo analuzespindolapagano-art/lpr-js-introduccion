43a91bafc8ba023eaf18aef83137ff12567989da
brisa scomazzon <analuzespindolapagano@gmail.com>
2026-05-15 16:45:29 -0300

    obtenersaludo

--- homework1.js
+++ homework1.js
@@ -222,7 +222,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
- 
+  return ("hola" + "" + "!");
   
  
   
